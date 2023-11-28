fa409d0778002559f6d2e81803dcd43d995aba6c
Karolis Ramanauskas <kramanauskas112@gmail.com>
2023-11-27 21:51:42 -0500

    finished form validation problems

--- contact-me.js
+++ contact-me.js
@@ -1,1 +1,72 @@
 // TODO
+const nameEl = document.getElementById("name");
+const email = document.getElementById("email");
+
+const form = document.getElementById("contact-me");
+
+const select = document.getElementById("contact-reason");
+const job = document.getElementById("job-title");
+const companyWebsite = document.getElementById("company-website");
+const codingLanguage = document.getElementById("coding-language");
+
+const message = document.getElementById("message");
+
+const validLength = (input, min) => {
+    if(input.value.trim().length > min){
+        input.parentElement.classList.remove("invalid");
+        return true;
+    } else {
+        input.parentElement.classList.add("invalid");
+        return false;
+    }
+};
+
+const validateEmail = (email) => {
+    const re = /\w+@\w+\.\w+/;
+    if (re.test(email.value.trim())){
+        email.parentElement.classList.remove("invalid");
+        return true;
+    } else {
+        email.parentElement.classList.add("invalid");
+        return false;
+    }
+};
+
+const handleSelect = (selectElement) => {
+    const selectedValue = selectElement.value;
+    if (selectedValue === "job"){
+        job.parentElement.classList.remove("hidden");
+        companyWebsite.parentElement.classList.remove("hidden");
+        codingLanguage.parentElement.classList.add("hidden");
+    } else if (selectedValue === "talk"){
+        codingLanguage.parentElement.classList.remove("hidden");
+        job.parentElement.classList.add("hidden");
+        companyWebsite.parentElement.classList.add("hidden");
+    }
+};
+
+select.addEventListener("change", () => handleSelect(select));
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let msg = '';
+    let valid = true;
+
+    if (!validLength(nameEl, 3)){
+        msg += '\nName must be included and > 3 characters.';
+        valid = false;
+    }
+    if (!validateEmail(email)){
+        msg += '\nEmail is not valid.';
+        valid = false;
+    }
+    if (!validLength(message, 10)){
+        msg += '\nMessage must be included and > 10 characters.';
+        valid = false;
+    }
+
+    if (!valid){
+        e.preventDefault();
+        alert(msg);
+    }
+});
--- form-validation-custom.js
+++ form-validation-custom.js
@@ -1,1 +1,55 @@
 // TODO
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const form = document.getElementById("connect-form");
+const select = document.getElementById("contact-kind");
+
+const os = document.getElementById("operating-system");
+const employees = document.getElementById("num-of-employees");
+
+let valid = false;
+
+const validLength = (input, min) => {
+    if(input.value.trim().length > min){
+        input.parentElement.classList.remove("invalid");
+        return true;
+    } else {
+        input.parentElement.classList.add("invalid");
+        return false;
+    }
+};
+
+const validateEmail = (email) => {
+    const re = /\w+@\w+\.\w+/;
+    if (re.test(email.value.trim())){
+        email.parentElement.classList.remove("invalid");
+        return true;
+    } else {
+        email.parentElement.classList.add("invalid");
+        return false;
+    }
+};
+
+const handleSelect = (selectElement) => {
+    const selectedValue = selectElement.value;
+    if (selectedValue === "business"){
+        employees.parentElement.classList.remove("hidden");
+        os.parentElement.classList.add("hidden");
+    } else if (selectedValue === "technical"){
+        os.parentElement.classList.remove("hidden");
+        employees.parentElement.classList.add("hidden");
+    }
+};
+
+select.addEventListener("change", () => handleSelect(select));
+
+form.addEventListener("submit", (e) => {
+    if (validLength(firstName, 3) && validLength(lastName, 3) && validateEmail(email)){
+        valid = true;
+    } else {
+        valid = false;
+        e.preventDefault();
+    }
+});
+
